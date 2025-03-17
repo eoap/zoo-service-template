1747a9f7f1add5d75c82fc56da7198f6820d14a1
Gérald Fenoy <gerald.fenoy@geolabs.fr>
2025-03-17 15:42:13 +0100

    Follow changes from pycalrissian

--- {{cookiecutter.service_name}}/service.py
+++ {{cookiecutter.service_name}}/service.py
@@ -271,7 +271,11 @@ class SimpleExecutionHandler(ExecutionHandler):
 
     def get_secrets(self):
         logger.info("get_secrets")
-        return self.local_get_file("/assets/pod_imagePullSecrets.yaml")
+        secrets={
+            "imagePullSecrets": self.local_get_file("/assets/pod_imagePullSecrets.yaml"),
+            "additionalImagePullSecrets": self.local_get_file("/assets/pod_additionalImagePullSecrets.yaml")
+        }
+        return secrets
 
 
 def {{cookiecutter.workflow_id |replace("-", "_")  }}(conf, inputs, outputs):  # noqa
